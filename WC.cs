3ba4bd612698528e00ef3ac0d7d39de6127a538d
Timi Odusanya <47190682+TimiOdusanya@users.noreply.github.com>
2023-04-01 02:33:56 +0100

    Add files via upload

---
+++ WC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportManagement
+{
+    public class WC
+    {
+        public const string ImagePath = @"\images\complaint\";
+
+        public const string VideoPath = @"\videos\complaint\";
+
+        public const string SessionCart = "ShoppingCartSession";
+
+        public const string AdminRole = "Admin";
+        public static string CustomerRole = "Customer";
+    }
+}
